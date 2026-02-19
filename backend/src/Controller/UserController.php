ed4ae9bd76ffa18f16a250e78949abb37a2ba35f
Alvin CARNEIRO <alvin.carneiro1@gmail.com>
2026-02-19 15:49:41 +0100

    Use UserService, add displayName, update routes
    Replace UserPresenter with UserService in UserController, add displayName to the /api/me JSON response, and adjust route naming. A class-level route name prefix ('api_') is added and endpoint route names were renamed (me and user_profile). The user profile action now injects UserService and uses its presentPublic method to build the response.

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -3,16 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Service\UserPresenter;
+use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/api')]
+#[Route('/api', name: 'api_')]
 final class UserController extends AbstractController
 {
-    #[Route('/me', name: 'api_me', methods: ['GET'])]
+    #[Route('/me', name: 'me', methods: ['GET'])]
     public function me(): JsonResponse
     {
         $user = $this->getUser();
@@ -23,6 +23,7 @@ final class UserController extends AbstractController
 
         return new JsonResponse([
             'username' => $user->getUsername(),
+            'displayName' => $user->getDisplayName(),
             'email' => $user->getEmail(),
             'roles' => $user->getRoles(),
             'createdAt' => $user->getCreatedAt()->format('Y-m-d H:i:s'),
@@ -33,6 +34,8 @@ final class UserController extends AbstractController
 
     #[Route('/user/{username}', name: 'api_user', methods: ['GET'])]
     public function getUserProfile(string $username, EntityManagerInterface $em, UserPresenter $presenter): JsonResponse
+    #[Route('/user/{username}', name: 'user_profile', methods: ['GET'])]
+    public function getUserProfile(string $username, EntityManagerInterface $em, UserService $userService): JsonResponse
     {
         $user = $em->getRepository(User::class)->findOneBy(['username' => $username]);
 
@@ -41,5 +44,7 @@ final class UserController extends AbstractController
         }
 
         return new JsonResponse($presenter->presentPublic($user));
+        return new JsonResponse($userService->presentPublic($user));
+    }
     }
 }
